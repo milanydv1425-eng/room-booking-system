7e59bb9dba02813c84849e8be0081e09ebd9609b
Milan Yadav <milanydv1425@gmail.com>
2026-03-06 15:16:36 +0530

    update

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,17 +1,23 @@
-import mysql from "mysql2";
+import mysql from "mysql2/promise";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-const db = mysql.createPool(process.env.DATABASE_URL);
-
-db.getConnection((err, connection) => {
-  if (err) {
-    console.log("Database connection failed:", err);
-  } else {
-    console.log("MySQL Connected");
-    connection.release();
-  }
+const db = mysql.createPool({
+  uri: process.env.DATABASE_URL,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+  enableKeepAlive: true,
+  keepAliveInitialDelay: 0
 });
+
+try {
+  const connection = await db.getConnection();
+  console.log("MySQL Connected");
+  connection.release();
+} catch (err) {
+  console.log("Database connection failed:", err);
+}
 
 export default db;
