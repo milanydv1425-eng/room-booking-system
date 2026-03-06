4e05bad5e316090ede40591914ba0ef29eab0067
Milan Yadav <milanydv1425@gmail.com>
2026-03-06 16:12:25 +0530

    fix render port

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ app.get("/", (req, res) => {
   res.send("Room Booking API Running");
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
